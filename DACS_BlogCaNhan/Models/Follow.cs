fc1ab3a7c0df3a508e37e5e78a1e850226de02cc
Tran The Bao <tranthebaobt8@gmail.com>
2024-04-23 09:25:58 +0700

    Add project files.

---
+++ DACS_BlogCaNhan/Models/Follow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DACS_BlogCaNhan.Models;
+
+public partial class Follow
+{
+    public long IdFollower { get; set; }
+
+    public long IdFollowing { get; set; }
+
+    public string? ExistenceStatus { get; set; }
+
+    public virtual User IdFollowerNavigation { get; set; } = null!;
+
+    public virtual User IdFollowingNavigation { get; set; } = null!;
+}
---
+++ DACS_BlogCaNhan/Models/RoleUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DACS_BlogCaNhan.Models;
+
+public partial class RoleUser
+{
+    public long IdRole { get; set; }
+
+    public string RoleName { get; set; } = null!;
+
+    public string DescriptionRole { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<User> Users { get; set; } = new List<User>();
+
+    public virtual ICollection<Permission> IdPermissions { get; set; } = new List<Permission>();
+}
